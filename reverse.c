b5220d28194fcd17c8bcd224d1a41c9431fd5d52
Wyatt Pearsall <wyattttttt@gmail.com>
2014-01-25 10:33:11 -0800

    Ch. 1

---
+++ reverse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse (char s[]);
+
+main{
+  char str[25]
+  int c, i=0
+  while((c=getchar()) != EOF){
+    str[i]=c;
+    i++
+  }
+  printf("Reversed: %s",reverse(str))
+}
+
+char[] reverse (char s[]){
+  int len, i, last, half;
+  char temp
+  len = strlen(s);
+  last = len-1
+  half = len/2;
+  for(i = 0; i <half; i++){
+    temp=s[i]
+    s[i]=s[last-i]
+    s[last-i]=temp;
+  }
+  return s
+  
+}
